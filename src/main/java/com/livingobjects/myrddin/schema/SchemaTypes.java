663ba2905ce63b0d87adcdfc273548582f987379
Michael Bitard <bitard.michael@gmail.com>
2015-03-24 18:21:44 +0100

    ugly hack in myrddin

--- src/main/java/com/livingobjects/myrddin/schema/SchemaTypes.java
+++ src/main/java/com/livingobjects/myrddin/schema/SchemaTypes.java
@@ -19,4 +19,45 @@ public enum SchemaTypes {
         }
         return result;
     }
+
+    // FIXME 2015-03-24: Ugly hack for handlebars
+    public boolean isRef() {
+        return is(REF);
+    }
+
+    public boolean isNumber() {
+        return is(NUMBER);
+    }
+
+    public boolean isInteger() {
+        return is(INTEGER);
+    }
+
+    public boolean isString() {
+        return is(STRING);
+    }
+
+    public boolean isBoolean() {
+        return is(BOOLEAN);
+    }
+
+    public boolean isObject() {
+        return is(OBJECT);
+    }
+
+    public boolean isArray() {
+        return is(ARRAY);
+    }
+
+    public boolean isAnyOf() {
+        return is(ANY_OF);
+    }
+
+    public boolean isOneOf() {
+        return is(ONE_OF);
+    }
+
+    private boolean is(SchemaTypes schemaTypes) {
+        return this == schemaTypes;
+    }
 }
